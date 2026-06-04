9ca9b00ccd6918dada2474d158f6053c40abd49a
Yash Sharma <yashsharma4527@gmail.com>
2026-06-04 15:07:14 +0530

    Day-8 uploaded

---
+++ Day-8/Ques31.java
@@ -0,0 +1,13 @@
+
+public class Ques31 {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            char ch = 'A';
+            for(int j=0; j<=i; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }    
+}
